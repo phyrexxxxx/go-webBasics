546a838bf6eb46ce43152aecdaa471a54799044a
phyrexxxxx <phyrexxxxx@gmail.com>
2023-09-14 02:32:02 +0800

    feat: implement basic file server and CORS middleware
    - Add a new file `fileserver.go` to the `Server/fileserver` directory
    - Implement a basic file server in `fileserver.go`
    - Set up a middleware for handling CORS in `fileserver.go`
    - Add a new file `index.html` to the `Server/fileserver` directory
    - Implement an HTML file for serving information about gophers in `index.html`
    - Rename the file `server.go` to `server/server.go` in the `Server` directory
    
    Signed-off-by: phyrexxxxx <phyrexxxxx@gmail.com>

---
+++ Server/fileserver/fileserver.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const currentDir = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir(currentDir))
+	mux.Handle("/", fs)
+	corsMux := middlewareCors(mux)
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+
+	log.Printf("Serving files from %s on port %s\n", currentDir, port)
+	log.Fatal(server.ListenAndServe())
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
